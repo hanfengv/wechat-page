1f40bb024361200bc71937e97d46719b41d097f6
hanfeng <hanfengv@gmail.com>
2021-07-12 00:06:58 +0800

    发布第一版

--- extension.js
+++ extension.js
@@ -1,25 +1,99 @@
 const vscode = require("vscode");
+const fs = require("fs");
+const path = require("path");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  const getCurFilePath = async () => {
-    return vscode.window.activeTextEditor.document.fileName;
+  /**
+   * 系统剪切板文字
+   * @returns
+   */
+  const getClipboard = async () => {
+    return await vscode.env.clipboard.readText();
   };
+
+  /**
+   * 当前编辑文件的路径
+   *
+   * @returns {object} - file
+   * @returns {string} - file.filePath
+   * @returns {string} - file.dirPath
+   */
+  const getCurFilePath = () => {
+    // editor.document.uri.fsPath;
+    const filePath = vscode.window.activeTextEditor.document.fileName;
+    const dirPath = path.dirname(filePath);
+    return {
+      filePath,
+      dirPath,
+    };
+  };
+
+  /**
+   * 获取文件夹下的文件
+   * @param {string} dirPath
+   * @returns {Object} file
+   * @returns {allFiles} file.allFiles 文件和文件夹
+   * @returns {files} file.files 仅文件
+   */
+  const getFloderFiles = (dirPath) => {
+    const isFile = (filePath) => {
+      return fs.lstatSync(filePath).isFile();
+    };
+
+    const allFiles = new Map();
+    fs.readdirSync(dirPath).forEach((fileName) => {
+      allFiles.set(fileName, path.join(dirPath, fileName));
+    });
+
+    const files = new Map(
+      [...allFiles].filter(([, filePath]) => isFile(filePath))
+    );
+
+    return {
+      allFiles,
+      files,
+    };
+  };
+
+  /**
+   * 打开文件
+   * @param {string} filePath
+   * @returns {Promise|boolean}
+   */
+  const quickOpen = async (filePath) => {
+    let fileUri = vscode.Uri.file(filePath);
+    vscode.window.showTextDocument(fileUri);
+    // return await vscode.commands.executeCommand("vscode.open", uri);
+  };
+
   const showPages = async () => {
-    // let editor = vscode.window.activeTextEditor;
-    // const curFileFsPath = editor.document.uri.fsPath;
-    const curFilePath = getCurFilePath();
-    const clipboardText = await vscode.env.clipboard.readText();
-    console.log("🚀 :: env:: ", { clipboardText, curFilePath });
-    const result = await vscode.window.showQuickPick(
-      ["a.wxss", "zwei", "drei"],
+    // const allOpenFileNames = vscode.TextEdit;
+    // console.log("🚀 :: showPages :: allOpenFileNames", allOpenFileNames)
+    // const activeTextEditor = vscode.window.activeTextEditor.document.fileName;
+    // console.log("🚀 :: showPages :: activeTextEditor", activeTextEditor);
+    // const visibleTextEditors = vscode.window.visibleTextEditors;
+    // const visDocs = visibleTextEditors.map(
+    //   (editor) => editor.document.fileName
+    // );
+    // console.log("🚀 :: showPages :: visDocs", visDocs);
+    const curFile = getCurFilePath();
+    const { files: floderFiles } = getFloderFiles(curFile.dirPath);
+
+    const chioseFileName = await vscode.window.showQuickPick(
+      [...floderFiles.keys()],
       {
         placeHolder: "请选择要打开的文件",
       }
     );
-    vscode.window.showInformationMessage(`Got: ${result}`);
+    console.log("🚀 :: showPages :: chioseFileName", chioseFileName);
+
+    const openedFileName = floderFiles.get(chioseFileName);
+    await quickOpen(openedFileName);
+
+    // vscode.window.showInformationMessage(`Got: ${openedFileName}`);
   };
 
   let helloWorldCommand = vscode.commands.registerCommand(
